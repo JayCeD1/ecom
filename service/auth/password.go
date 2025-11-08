9af9942446e17ef7e5e4b01245bea8d0df3e422e
jayced1 <jesseeren@gmail.com>
2025-11-08 22:53:49 +0200

    add ComparePasswords function for verifying hashed passwords

--- service/auth/password.go
+++ service/auth/password.go
@@ -6,3 +6,8 @@ func HashPassword(password string) (string, error) {
 	pass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(pass), err
 }
+
+func ComparePasswords(hashed string, plain []byte) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), plain)
+	return err == nil
+}
