d8aff6ddfb3d37a7d01ab0de2a138675d9ef8bea
jayced1 <jesseeren@gmail.com>
2025-11-08 22:29:37 +0200

    implement GetUserByID and CreateUser methods in user store

--- service/user/store.go
+++ service/user/store.go
@@ -25,13 +25,19 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 		return nil, fmt.Errorf("user not found")
 	}
 	return &user, nil
-
 }
 
 func (s *Store) GetUserByID(id int) (*types.User, error) {
-	return nil, nil
+	var user types.User
+	s.db.Where("id = ?", id).First(&user)
+
+	if user.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+	return &user, nil
 }
 
 func (s *Store) CreateUser(user *types.User) error {
+	s.db.Create(&user)
 	return nil
 }
