e3da2c296203682ebe436ee7d7112facbc30709e
jayced1 <jesseeren@gmail.com>
2025-11-10 09:58:12 +0200

    extend AutoMigrate to include Order and OrderItem types

--- db/db.go
+++ db/db.go
@@ -14,7 +14,7 @@ func NewSQLStorage(cfg postgres.Config) (*gorm.DB, error) {
 		log.Fatal(err)
 	}
 
-	er := db.AutoMigrate(&types.User{}, &types.Product{})
+	er := db.AutoMigrate(&types.User{}, &types.Product{}, &types.Order{}, &types.OrderItem{})
 	if er != nil {
 		log.Fatal(er)
 	}
