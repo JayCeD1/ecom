541cfa99b388db15d5e3c6ac99bfe3516de87043
jayced1 <jesseeren@gmail.com>
2025-11-10 09:57:57 +0200

    refactor error handling in cart service to simplify variable naming and improve readability

--- service/cart/routes.go
+++ service/cart/routes.go
@@ -1,23 +1,66 @@
 package cart
 
 import (
+	"ecom/service/auth"
 	"ecom/types"
+	"ecom/utils"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type Handler struct {
-	store types.OrderStore
+	store        types.OrderStore
+	productStore types.ProductStore
+	userStore    types.UserStore
 }
 
-func NewHandler(store types.OrderStore) *Handler {
-	return &Handler{store: store}
+func NewHandler(store types.OrderStore, productStore types.ProductStore, userStore types.UserStore) *Handler {
+	return &Handler{
+		store:        store,
+		productStore: productStore,
+		userStore:    userStore,
+	}
 }
 
 func (h *Handler) RegisterRoutes(r fiber.Router) {
-	r.Post("/checkout", h.Checkout)
+	r.Post("/checkout", auth.WithJWT(h.Checkout, h.userStore))
 }
 
 func (h *Handler) Checkout(c *fiber.Ctx) error {
-	return nil
+	userID, err := auth.GetUserIDFromContext(c)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	var cart types.CartCheckoutRequest
+	if err := c.BodyParser(&cart); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	if err := utils.Validate.Struct(cart); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	// get products
+	productIDs, err := getCartItemIDs(cart.Items)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	products, err := h.productStore.GetProductsByID(productIDs)
+	// create order
+	orderID, amount, err := h.createOrder(products, cart.Items, userID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"orderID": orderID,
+		"amount":  amount,
+	})
 }
