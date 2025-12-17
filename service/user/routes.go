0505bf7b85d90e316ae4ffdb55f0bb25493cb75b
jayced1 <jesseeren@gmail.com>
2025-12-17 23:27:16 +0300

    refactor: reorder `GetUserByEmail` params and inject context into `CreateUser` for improved request handling

--- service/user/routes.go
+++ service/user/routes.go
@@ -37,7 +37,7 @@ func (h *Handler) Login(c *fiber.Ctx) error {
 			"error": err.Error(),
 		})
 	}
-	user, err2 := h.store.GetUserByEmail(payload.Email, c.UserContext())
+	user, err2 := h.store.GetUserByEmail(c.UserContext(), payload.Email)
 
 	if err2 != nil {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
@@ -80,7 +80,7 @@ func (h *Handler) Register(c *fiber.Ctx) error {
 	}
 
 	//check if the user exists
-	if _, err := h.store.GetUserByEmail(payload.Email, c.UserContext()); err == nil {
+	if _, err := h.store.GetUserByEmail(c.UserContext(), payload.Email); err == nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "user already exists",
 		})
@@ -94,7 +94,7 @@ func (h *Handler) Register(c *fiber.Ctx) error {
 		})
 	}
 
-	err2 := h.store.CreateUser(&types.User{
+	err2 := h.store.CreateUser(c.UserContext(), &types.User{
 		FirstName: payload.FirstName,
 		LastName:  payload.LastName,
 		Email:     payload.Email,
